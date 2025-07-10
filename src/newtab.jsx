8cf416514847c0171a19402535e5beaff35da03e
abearman <amylbearman@gmail.com>
2025-07-09 17:45:37 -0700

    Got user accounts showing up in Cognito

--- src/newtab.jsx
+++ src/newtab.jsx
@@ -272,10 +272,43 @@ function NewTabUI({ user, signIn, signOut}) {
   );
 }
 
+// Define the custom form fields object
+const formFields = {
+  signUp: {
+    given_name: {
+      label: "First Name",
+      placeholder: "Enter your first name",
+      isRequired: true,
+      order: 1,
+    },
+    family_name: {
+      label: "Last Name",
+      placeholder: "Enter your last name",
+      isRequired: true,
+      order: 2,
+    },
+    email: {
+      order: 3,
+    },
+    phone_number: {
+      label: "Phone Number",
+      placeholder: "Enter your phone number",
+      isRequired: true,
+      order: 4,
+    },
+    password: {
+      order: 5
+    },
+    confirm_password: {
+      order: 6
+    },
+  },
+};
+
 ReactDOM.render(
   <React.StrictMode>
     <AppContextProvider>
-      <Authenticator>
+      <Authenticator formFields={formFields}>
         {({ signIn, signOut, user }) => (
           <NewTabUI user={user} signIn={signIn} signOut={signOut} />
         )}
