0d6f7308e05613ecc9b69c38719404eff9e8da37
abearman <amylbearman@gmail.com>
2025-10-20 18:04:29 -0700

    Fixed error where Phone number format is not letting the user submit

--- src/config/formFields.js
+++ src/config/formFields.js
@@ -4,13 +4,13 @@ const formFields = {
     given_name: {
       label: "First Name",
       placeholder: "Enter your first name",
-      isRequired: true,
+      required: true,
       order: 1,
     },
     family_name: {
       label: "Last Name",
       placeholder: "Enter your last name",
-      isRequired: true,
+      required: true,
       order: 2,
     },
     email: {
@@ -18,9 +18,8 @@ const formFields = {
     },
     phone_number: {
       label: "Phone Number",
-      labelHidden: true,
       placeholder: "Enter your phone number",
-      isRequired: true,
+      required: true,
       order: 4,
     },
     password: {
